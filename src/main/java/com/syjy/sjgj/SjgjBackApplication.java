9d88ec630ad3b2b5730426e698c6e8fdf0c7fa6f
tmddus2123 <tmddus2123@gmail.com>
2025-01-12 16:02:36 +0900

    1) sjgj_back init

---
+++ src/main/java/com/syjy/sjgj/SjgjBackApplication.java
@@ -0,0 +1,13 @@
+package com.syjy.sjgj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SjgjBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SjgjBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/syjy/sjgj/SjgjBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syjy.sjgj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SjgjBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
